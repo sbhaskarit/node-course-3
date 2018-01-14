894733366ecb2bf6f7213f4b5875a0490af7f47f
Shailesh Bhaskar <EVOLVUS\shaileshb@elpblr-045.evolvus.com>
2018-01-14 18:58:15 +0530

    Setup start script and Heroku Part

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use((req,res,next) => {
   next();
 });
 
-app.use((req,res,next) => {
+/*app.use((req,res,next) => {
   res.render('maintanance.hbs');
-});
+});*/
 
 
 hbs.registerHelper('getCurrentYear',()=>{
